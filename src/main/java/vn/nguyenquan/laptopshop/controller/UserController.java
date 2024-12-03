f68e9f85fffac387e487f41fe271c881c488392d
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2024-12-03 20:41:47 +0700

    Code DI

---
+++ src/main/java/vn/nguyenquan/laptopshop/controller/UserController.java
@@ -0,0 +1,26 @@
+package vn.nguyenquan.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.nguyenquan.laptopshop.service.UserSevice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class UserController {
+
+    private UserSevice userSevice;
+
+    public UserController(UserSevice userSevice) {
+        this.userSevice = userSevice;
+    }
+
+    @GetMapping("")
+    public String getHomePage() {
+        return this.userSevice.handleHello();
+    }
+}
---
+++ src/main/java/vn/nguyenquan/laptopshop/service/UserSevice.java
@@ -0,0 +1,11 @@
+package vn.nguyenquan.laptopshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserSevice {
+
+    public String handleHello() {
+        return "Hello form service";
+    }
+}
